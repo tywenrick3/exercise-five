f780ebd0b601dd0104ce20501cb3afd8ecf397bd
Ty Wenrick <tew287@nyu.edu>
2021-11-18 09:41:37 -0500

    netlify should break

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+// Page Imports
 import Login from './pages/Login';
 import CreateUser from './pages/CreateUser';
 import Header from './components/Header';
 import UserProfile from './pages/UserProfile';
+import FirebaseConfig from './components/FirebaseConfig';
 
 function App() {
+	// Track if user is logged in
+	const [loggedIn, setLoggedIn] = useState(false);
+	// Check to see if there is any loading
+	const [loading, setLoading] = useState(true);
+	// Store user information in state
+	const [userInformation, setUserInformation] = useState({});
+	const [appInitialized, setAppInitialized] = useState(false);
+
+	useEffect(() => {
+		initializeApp(FirebaseConfig);
+		setAppInitialized(true);
+	}, []);
+
+	useEffect(() => {
+		if (appInitialized) {
+			const auth = getAuth();
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setUserInformation(user);
+					setLoggedIn(true);
+				} else {
+					setUserInformation({});
+					setLoggedIn(false);
+				}
+			});
+			setLoading(false);
+		}
+	}, [appInitialized]);
+
+	if (loading) return null;
+
 	return (
 		<>
 			<Header />
 			<Router>
 				<Routes>
 					<Route path='/user/:id' element={<UserProfile />} />
-					<Route path='/create' element={<CreateUser />} />
+					<Route
+						path='/create'
+						element={
+							<CreateUser
+								setLoggedIn={setLoggedIn}
+								setUserInformation={setUserInformation}
+							/>
+						}
+					/>
 					<Route path='/' element={<Login />} />
 				</Routes>
 			</Router>
--- src/components/CreateUserForm.js
+++ src/components/CreateUserForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function CreateUserForm() {
+function CreateUserForm({ signUpUser }) {
 	return (
 		<div>
 			<h2>Create User Form</h2>
+			<form onSubmit={(e) => signUpUser(e)}>
+				<label htmlFor='email'>Email</label>
+				<input type='email' name='email' placeholder='Enter Email' />
+
+				<label htmlFor='password'>Password</label>
+				<input type='password' name='password' placeholder='****' />
+
+				<button type='submit'>Create User</button>
+			</form>
 		</div>
 	);
 }
